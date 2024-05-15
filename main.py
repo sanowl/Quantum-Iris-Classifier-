a5b84795ae2ee652095cb9f0a7cd42a8b7e0ab5f
San <99511815+sanowl@users.noreply.github.com>
2024-05-15 09:16:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
 from qiskit.circuit.library import ZZFeatureMap, RealAmplitudes
-from qiskit.aqua import QuantumInstance
-from qiskit.aqua.algorithms import VQC
-from qiskit.aqua.components.optimizers import SPSA
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import VQC
+from qiskit.algorithms.optimizers import SPSA
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
-
 
 iris = load_iris()
 X, y = iris.data, iris.target
@@ -20,8 +19,13 @@ num_qubits = 2
 feature_map = ZZFeatureMap(feature_dimension=num_qubits, reps=2, entanglement='linear')
 var_form = RealAmplitudes(num_qubits, reps=3)
 
-
-
 # Set up the variational quantum classifier
-vqc = VQC(feature_map=feature_map, var_form=var_form, optimizer=SPSA(max_trials=100),
+vqc = VQC(feature_map=feature_map, ansatz=var_form, optimizer=SPSA(maxiter=100),
           quantum_instance=QuantumInstance(Aer.get_backend('qasm_simulator'), shots=1024))
+
+vqc.fit(X_train, y_train)
+
+# Evaluate the classifier on the test set
+y_pred = vqc.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.3f}")
