7be5d949904f3ad3424b188c0d736b16ffee71c4
San <99511815+sanowl@users.noreply.github.com>
2024-05-15 09:12:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+
+
+iris = load_iris()
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
